faf1648dfbc42b777096c5909ad21db64a1a3047
shehan Fernando <shehand@procons.lk>
2021-02-15 22:36:53 +0530

    shehan first commit

--- project/app/src/main/java/com/itx360/myapplication/MainActivity.java
+++ project/app/src/main/java/com/itx360/myapplication/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+//shehan
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
