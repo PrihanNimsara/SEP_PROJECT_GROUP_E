20735f8b2855cd43f56fe4a8104cf8f2fa0ee83b
Prihan Nimsara <prihann@itx360.com>
2021-02-28 01:51:44 +0530

    base configuraion and the merge the modification level one

--- project/app/src/main/java/com/itx360/myapplication/MainActivity.java
+++ project/app/src/main/java/com/itx360/myapplication/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //test
         //this is a button function
+        //
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
