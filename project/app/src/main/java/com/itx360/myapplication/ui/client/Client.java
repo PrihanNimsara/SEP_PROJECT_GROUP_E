0ffb94822c532b49828a83cb76312805e5040543
Prihan Nimsara <prihann@itx360.com>
2021-03-21 11:55:22 +0530

    cleint detail screen - logic change

--- project/app/src/main/java/com/itx360/myapplication/ui/client/Client.java
+++ project/app/src/main/java/com/itx360/myapplication/ui/client/Client.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 public class Client extends Fragment {
 @Override
 public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return null;
         }
         }
