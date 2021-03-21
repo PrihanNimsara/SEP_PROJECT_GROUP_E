2609e5581029016d55b1a8f9f071dedf0ed7f23b
123janani <32632854+123janani@users.noreply.github.com>
2021-03-21 11:39:03 +0530

    Client Adapter update

---
+++ project/app/src/main/java/com/itx360/myapplication/ui/client/Client.java
@@ -0,0 +1,15 @@
+package com.itx360.myapplication.ui.client;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//#parse("File Header.java")
+public class Client extends Fragment {
+@Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+        }
+        }
