1821957c3a8687653f1eaf33f7271ff2d8fa1084
PrihanNimsara <Iamprihan@gmail.com>
2021-04-08 00:37:52 +0530

    shehan code review

--- project/app/src/main/java/com/itx360/myapplication/MainActivity.java
+++ project/app/src/main/java/com/itx360/myapplication/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
-janani
+shehan
 
         setSupportActionBar(toolbar);
         //test
